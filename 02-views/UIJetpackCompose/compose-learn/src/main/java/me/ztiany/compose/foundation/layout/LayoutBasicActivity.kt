84f9cb3497dffc56d62ec96ba760101a2c786ec0
Alien <ztiany3@gmail.com>
2021-10-29 18:01:13 +0800

    update compose

--- 02-views/UIJetpackCompose/compose-learn/src/main/java/me/ztiany/compose/foundation/layout/LayoutBasicActivity.kt
+++ 02-views/UIJetpackCompose/compose-learn/src/main/java/me/ztiany/compose/foundation/layout/LayoutBasicActivity.kt
@@ -3,10 +3,12 @@ package me.ztiany.compose.foundation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ComposeFoundationActivity : AppCompatActivity() {
+class Foundation01_LayoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+
 
 }
